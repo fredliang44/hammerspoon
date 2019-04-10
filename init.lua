ae6580b2522288062d03558a05db4d09cb73ec30
fredliang44 <fredliang@live.com>
2019-04-10 09:10:41 +0800

    init project

---
+++ init.lua
@@ -0,0 +1,7 @@
+print('==================================================')
+require "modules/hotkey"
+require "modules/screens"
+require "modules/windows"
+require "modules/launch"
+require "modules/system"
+require "modules/auto_reload"
